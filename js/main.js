58ca42bd02c837ceb0438a815b894e99250dee29
CallMeLance <lance.franz33@gmail.com>
2019-09-26 10:03:39 -0500

    Finished the quiz size

--- js/main.js
+++ js/main.js
@@ -1,28 +1,21 @@
-/* 
+/*
     This is an IIFE - Immediately Invoked Function Expression.
-    IIFE's run as soon as their defined. IIFE's are commonly used to keep variables and functions outside of the globalscope, and they tend to work better if you're running multiple scripts
-
-    */
-
-// This variable exists in the global scope, which it means it's less secure and can be accessed by anything
-
+    IIFEs run as soon as they are defined. IIFEs are commonly used to keep variables/functions outside of the global scope and they tend to work better if you're running multiple scripts.
+*/
+//This variable exists in the global scope, which means it's less secure and can be accessed by anything
 var global;
-
 (function() {
-    // Because this variable is inside of a IIFE, it is not in the globalscope
+    //Because this variable is inside of an IIFE, it is not in the global scope
     var variable = 10;
-
-    // Make references to our elements that we're going to interact with
+    
+    //Make references to our elements that we're going to interact with
     var quizContainer = document.getElementById("quiz");
     var resultsContainer = document.getElementById("results");
-    var submitContainer = document.getElementById("submit");
-    var resetContainer = document.getElementById("reset");
-
-    var myQuestions = [],
-
-    // What does a quiz question consist of?
-    // Question text, answer choices, correct answer
-
+    var submitButton = document.getElementById("submit");
+    var resetButton = document.getElementById("reset");
+    var myQuestions = [];
+    //What does a quiz question consist of?
+    //Question Text, Answer Choices, Correct Answer
     var question1 = {
         question: "What color is the sky?",
         answers: {
@@ -32,46 +25,115 @@ var global;
         },
         correctAnswer: "a"
     }
-
-    console.log(question1.question); // Get the question text
-    console.log(question.answers.b); // Get answer b
-
+    console.log(question1.question); //Get the question text
+    console.log(question1.answers.b); //Get answer b
     var question2 = {
-        question: "How many seconds in 5 minutes",
+        question: "What is the capital city of Peru?",
         answers: {
-            a: "60",
-            b: "300",
-            c: "600"
+            a: "Lima",
+            b: "Cusco",
+            c: "No idea"
         },
-        correctAnswer: "b"
-        
+        correctAnswer: "a"
     }
-
-    console.log(question2.question);
-    console.log(question.answers.c);
-
     var question3 = {
-        question: "What is the country just south of the United States?",
+        question: "Where is Waldo really?",
         answers: {
-            a: "Canada",
-            b: "Mexico",
-            c: "Oklahoma"
-        },
-        correctAnswer: "b"
+            a: "Antarctica",
+            b: "Exploring the Pacific Ocean",
+            c: "Sitting in a tree",
+            d: "Minding his own business, so stop asking"
+        }, 
+        correctAnswer: "d"
     }
-    // Add the question objects to our array of questions
-    myQuestions.push(question1,question2, question3);
-
-    // Function to build a quiz that goes through our question objects and generates HTML for each question
+    //Add the question objects to our array of questions
+    myQuestions.push(question1, question2, question3);
+    //Function to build a quiz that goes through our question objects and generates HTML for each question
     function buildQuiz() {
-
+        //We need to go through each of our question objects and use them to build out the HTML to show a question
+        for (var i = 0; i < myQuestions.length; i++) {
+            //Create a display for the question text
+            //Creating a new div called questionDiv that will hold information about a single question
+            var questionDiv = document.createElement("div");
+            //Get the question text from the question we are looking at with this iteration of the loop
+            questionDiv.innerText = myQuestions[i].question;
+            //Display the answer choices (and take user input to select an answer)
+            //Create a div to hold the question answers
+            var answersDiv = document.createElement("div");
+            answersDiv.classList.add("answers");
+            
+            //For each property in the current question's answers object
+            /* A for-in loop is used to go through the properties of an object */
+            for (letter in myQuestions[i].answers) {
+                //Create the label for the radio button input
+                var label = document.createElement("label");
+                //Create a radio button for each answer for this question
+                var input = document.createElement("input");
+                //Configure the input element
+                input.type = "radio";
+                input.name = "question" + i;
+                input.value = letter;
+                //Add the input to our label
+                label.appendChild(input);
+                //Create some text from the current letter we're looking at and the corresponding answer for that letter
+                var labelText = document.createTextNode(`${letter} : ${myQuestions[i].answers[letter]}`);
+                //Add the text to the label
+                label.appendChild(labelText);
+                //Add the label to the answers div
+                answersDiv.appendChild(label);
+            }
+            //Once all the answers are added, add the answerDiv to the questionDiv
+            questionDiv.appendChild(answersDiv);
+            //Add the questionDiv to the quizContainer
+            quizContainer.appendChild(questionDiv);
+        }
     }
-    // Function to show the results of the quiz
+    buildQuiz();
+    //Function to show the results of the quiz
     function showResults() {
-
-    }
-    // Function to reset the quiz
-    function resetQuiz() {
+        //Get all the answer containers from our quiz
+        var answerContainers = quizContainer.querySelectorAll(".answers"); //This will basically give us back an array containing everything in the quizContainer with the class "answers"
         
+        // Variable to keep track of how many they get right
+        var numCorrect = 0;
+
+
+        for (var i = 0; i < answerContainers.length; i++) {
+            //Get the current answer container we're looking at for the loop
+            var answerContainer = answerContainers[i];
+            //String to find which input is checked for the current question
+            var selector = `input[name=question${i}]:checked`;
+            //Try to find the selected answer in the container and print out the value
+            var userAnswer = (answerContainer.querySelector(selector) || {}).value; //If it can't find a selected input for a question, querySelector will give back null. If it does, use an empty object instead, since {}.value gives back undefined instead of null
+            if (userAnswer === myQuestions[i].correctAnswer) {
+                //They got it right!
+                // Add 1 to numCorrect
+                numCorrect++;
+                //Change the text color of the elements in the answer container to green
+                answerContainer.style.color = "green";
+            } else {
+                //They got it wrong!
+                //Change the text color of the elements in the answer contain to red
+                answerContainer.style.color = "red";
+            }
+        }
+
+        // Add text to the results container to show how many they got right
+        resultsContainer.innerText = "You got " + numCorrect + " out of " + myQuestions.length
     }
+    //Call the showResults function when the submit button is clicked
+    submitButton.addEventListener("click", showResults);
+
+    //Function to reset the quiz
+    function resetQuiz() {
+        // Clear out what's in the results container
+        resultsContainer.innerText = "";
+        // Clear out the quiz container
+        quizContainer.innerHTML = "";
+        // Rebuild the quiz
+        buildQuiz();
+    }
+
+    // Call the resetQuiz function when the reset button is clicked
+    resetButton.addEventListener("click", resetQuiz);
 })();
